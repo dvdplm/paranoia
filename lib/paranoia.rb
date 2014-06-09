7d1362fa91097b33d40339a7b5491a54ef4b8256
David Palm <dvdplm@gmail.com>
2014-06-09 16:04:32 +0200

    Let Arel do the quoting to fix breakage on Postgres

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -171,7 +171,7 @@ module ActiveRecord
       protected
       def build_relation_with_paranoia(klass, table, attribute, value)
         relation = build_relation_without_paranoia(klass, table, attribute, value)
-        relation.and(klass.quoted_table_name + ".#{klass.paranoia_column} IS NULL")
+        relation.and(table[klass.paranoia_column].eq(nil))
       end
       alias_method_chain :build_relation, :paranoia
     end
